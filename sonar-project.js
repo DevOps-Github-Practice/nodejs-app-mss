65fb36ea7f325aca199dfad9f24cba9dd896de3f
vkpatilhsit <100131108+vkpatilhsit@users.noreply.github.com>
2022-03-31 16:06:34 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://http://3.110.215.242:9000/',
+  serverUrl: 'http://43.204.24.67:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
